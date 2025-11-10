aa1bb0e25efc6ed5adc0365b515e6eb90a121969
Branko <banne_nikolic@hotmail.com>
2025-11-10 22:17:05 +0100

    Initial commit

---
+++ src/main/java/se/brankoov/routing/RoutingBackendApplication.java
@@ -0,0 +1,13 @@
+package se.brankoov.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/brankoov/routing/RoutingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.brankoov.routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
